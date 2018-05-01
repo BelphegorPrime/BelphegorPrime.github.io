0ac014413efb39e6ecf18dafb5c4502d98cc7724
marcel <roesler1@gmx.de>
2018-05-01 20:14:27 +0200

    added router, eslint and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import { Provider } from 'react-redux';
+import { Router, Route, Switch } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { createBrowserHistory } from 'history';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 import App from './App';
+
+import store from './store';
+import './styles/index.css';
+import './styles/print.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+
+class Root extends Component {
+  render() {
+    return (
+      <div>
+        <Provider store={store}>
+          <Router history={history}>
+            <Switch>
+              <Route path="/" component={App} />
+            </Switch>
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
