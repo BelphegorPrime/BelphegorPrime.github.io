0e34c611d4465af2bbec33d0282a110f4e8a5469
marcel <roesler1@gmx.de>
2018-10-31 23:49:56 +0100

    added resumedata

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import resumeData from '../assets/resumeData';
 
 const Resume = props => {
-  console.log(props);
+  const { firstname, lastname, email, github, twitter } = resumeData;
+  const experiences = resumeData.experiences
+    .map(d => {
+      const to = Date.parse(d.to);
+      return {
+        ...d,
+        from: Date.parse(d.from),
+        to: isNaN(to) ? new Date().getTime() : to
+      };
+    })
+    .sort((a, b) => {
+      if (a.from > b.from) {
+        return 1;
+      }
+      if (a.from <= b.from) {
+        return -1;
+      }
+      return 0;
+    });
   return <div className="App" />;
 };
 
