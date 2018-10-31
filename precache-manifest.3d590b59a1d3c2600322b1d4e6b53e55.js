8b0ccac59fc0016b913f0b5706b1117c5d150902
marcel <roesler1@gmx.de>
2018-10-31 22:54:49 +0100

    Updates

---
+++ precache-manifest.3d590b59a1d3c2600322b1d4e6b53e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "787d8215e62201525607",
+    "url": "/static/css/main.77ff5923.chunk.css"
+  },
+  {
+    "revision": "787d8215e62201525607",
+    "url": "/static/js/main.787d8215.chunk.js"
+  },
+  {
+    "revision": "465aec38d63261b0ecd8",
+    "url": "/static/css/1.2996acd4.chunk.css"
+  },
+  {
+    "revision": "465aec38d63261b0ecd8",
+    "url": "/static/js/1.465aec38.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a08eb8d2a765e6e00dc8d99c8c1a68aa",
+    "url": "/index.html"
+  }
+];
